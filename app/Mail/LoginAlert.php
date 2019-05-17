60beb8cf7e55fb47744a35a2f845d0d38ec52b98
eadortsu <mail@eadortsu.com>
2019-05-17 14:04:45 +0000

    Init

---
+++ app/Mail/LoginAlert.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\User;
+
+class LoginAlert extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        //
+        $this->user = $user;
+
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('no-reply@RuperMurder.com','RuperMurder')
+            ->subject('Login Alert')
+            ->view('email.login_alert');
+    }
+}
---
+++ resources/views/email/login_alert.blade.php
@@ -0,0 +1,5 @@
+<h2>Dear {{$user->name}},</h2><br>
+<p>There have been there have been multiple login attempts on your account</p><br>
+<p>If you did not trying logging in contact support or use this link to <a  href="{{ route('password.request') }}">reset your password</a></p><br>
+
+<br>
